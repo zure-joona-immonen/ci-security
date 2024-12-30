df2c92cdef42dca1c607935a5629e15bf2be9885
Rinorragi <rinorragi@gmail.com>
2024-12-30 16:52:48 +0200

    Add lab05

--- scripts/licenseaudit.ps1
+++
@@ -1,4 +0,0 @@
-$licenseJson = dotnet delice .\ci-security.sln --json | ConvertFrom-Json
-$ReturnArray = @()
-$licenseJson.Projects | ForEach-Object { $_.licenses | Select-Object -ExpandProperty expression | ForEach-Object { $ReturnArray += $_ } }
-$ReturnArray
