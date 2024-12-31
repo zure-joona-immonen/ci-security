2b1987bda19ad8bd7e4f0366c988dbcc189eaf4b
Rinorragi <rinorragi@gmail.com>
2024-12-31 14:10:58 +0200

    Lab02 - Rights for ci-pipeline

--- scripts/branchprotections.ps1
+++ scripts/branchprotections.ps1
@@ -1,29 +1,35 @@
 # Check the fields you want from https://docs.github.com/en/graphql/reference/objects#branchprotectionrule
 $query = @"
-{repository(owner:"Rinorragi", name:"ci-security") {
-branchProtectionRules(first:100){
-  nodes{
-    pattern,
-    allowsDeletions,
-    allowsForcePushes,
-    blocksCreations,
-    dismissesStaleReviews,
-    lockBranch,
-    requireLastPushApproval,
-    requiredApprovingReviewCount,
-    requiredDeploymentEnvironments,
-    requiredStatusCheckContexts,
-    requiresApprovingReviews,
-    requiresCodeOwnerReviews,
-    requiresCommitSignatures,
-    requiresConversationResolution,
-    requiresDeployments,
-    requiresLinearHistory,
-    requiresStatusChecks,
-    requiresStrictStatusChecks,
-    restrictsPushes,
-    databaseId}
-}}}
+{
+  repository(owner:"Rinorragi", name:"ci-security") {
+    branchProtectionRules(first:100) {
+      edges {
+        node {
+          pattern,
+          allowsDeletions,
+          allowsForcePushes,
+          blocksCreations,
+          dismissesStaleReviews,
+          lockBranch,
+          requireLastPushApproval,
+          requiredApprovingReviewCount,
+          requiredDeploymentEnvironments,
+          requiredStatusCheckContexts,
+          requiresApprovingReviews,
+          requiresCodeOwnerReviews,
+          requiresCommitSignatures,
+          requiresConversationResolution,
+          requiresDeployments,
+          requiresLinearHistory,
+          requiresStatusChecks,
+          requiresStrictStatusChecks,
+          restrictsPushes,
+          databaseId
+        }
+      }
+    }
+  }
+}
 "@
 
 gh api graphql -f query="$query"
